f8648ad0fea50f80e4579d6706932a152bbd071a
priyank shah <priyankshah0101@gmail.com>
2025-06-17 12:31:05 -0400

    Updated Hero file path

--- dev-portfolio/src/components/Hero.jsx
+++ dev-portfolio/src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Hero.css';
-import profileImage from '../images/Me.jpg'; // Update this path if needed
+import profileImage from '../data/images/Me.jpg'; // Update this path if needed
 
 const Hero = () => {
   return (
